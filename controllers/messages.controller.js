b90ff6cae4dd5b1334e803157b9cedd053ccb01a
Arnold <A2461089@KATYISD.org>
2024-11-08 09:51:47 -0600

    finished example

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const friends = require('../models/friends.model');
 
 function getMessages(req, res) {
-  res.sendFile( path.join(__dirname, '..', 'public','images', 'skimountain.jpg'));
+  res.render('messages', {
+    title: 'Messages to my Friends!',
+    friend: 'Elon Musk',
+  })
+  // res.sendFile( path.join(__dirname, '..', 'public','images', 'skimountain.jpg'));
 }
 
 function postMessage(req, res){
